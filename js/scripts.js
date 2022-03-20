ed2f206ebbd0472c76554e9a2d1b812e8411d59f
Robert <rgitonga.07@gmail.com>
2022-03-21 00:27:01 +0300

    add functionality to order button

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,35 @@
-$('table').hide();
+$(document).ready(function(){
+    $('table').hide();
+    $('.more-buttons').hide();
 
-$('.order').click(function(){
-    var pizzaSize = 
+    // adding functionality to order button
+
+    $('.order').click(function(){
+        var pizzaSize = $('.size option:selected').val();
+        var pizzaToppings = $('.toppings option:selected').val();
+        var pizzaCrust = $('.crust option:selected').val();
+        var total = parseInt(pizzaSize) + parseInt(pizzaToppings) + parseInt(pizzaCrust);
+        console.log(total);
+    })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
