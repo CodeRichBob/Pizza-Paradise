1859d176b254f164bf19f1e596a34f754ae74f76
Robert <rgitonga.07@gmail.com>
2022-03-21 01:56:12 +0300

    adding table data

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,25 @@ $(document).ready(function(){
     // adding functionality to order button
 
     $('.order').click(function(){
+        var # = 1
         var pizzaSize = $('.size option:selected').val();
         var pizzaToppings = $('.toppings option:selected').val();
         var pizzaCrust = $('.crust option:selected').val();
         var total = parseInt(pizzaSize) + parseInt(pizzaToppings) + parseInt(pizzaCrust);
         console.log(total);
+        var subTotal = 0
 
         // display table
     $('table').show();
     $('.more-buttons').show();
     $('.order').hide();
     })
+
+    //adding table data
+    $('#size').html($('.size option:selected').text());
+    $('#crust').html($('.crust option:selected').text());
+    $('#toppings').html($('.toppings option:selected').text());
+    $('#total').html(total);
 })
 
 
