0ea54b435ce52c652c3ee7b478dd317a7ad2660f
Robert <rgitonga.07@gmail.com>
2022-03-21 04:26:40 +0300

    defining parameter totalPrice

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
         var pizzaToppings = $('.toppings option:selected').val();
         var pizzaCrust = $('.crust option:selected').val();
         var totalPrice = parseInt(pizzaSize) + parseInt(pizzaToppings) + parseInt(pizzaCrust);
-        console.log(totalPrice);
+        //console.log(totalPrice);
         var subTotal = 0
 
         // display table
@@ -24,7 +24,32 @@ $(document).ready(function(){
     $('#crust').html($('.crust option:selected').text());
     $('#toppings').html($('.toppings option:selected').text());
     $('#total').html(totalPrice);
-    })
+
+    //creating a function to create pizza
+
+    function pizzaOrder (size,toppings,crust,item,totalPrice){
+        this.size = size;
+        this.toppings = toppings;
+        this.crust = crust;
+        this.item = item
+        this.totalPrice = totalPrice
+    }
+    $('.addPizza').click(function(){
+        var pizzaSize = $('.size option:selected').val();
+        var pizzaToppings = $('.toppings option:selected').val();
+        var pizzaCrust = $('.crust option:selected').val();
+        var totalPrice = parseInt(pizzaSize) + parseInt(pizzaToppings) + parseInt(pizzaCrust);
+        subTotal = subTotal + totalPrice
+        item = item + 1
+        
+        var newPizzaOrder = new pizzaOrder(pizzaSize,pizzaCrust,pizzaToppings,item,totalPrice)
+        var orderDetails = '<tr><th scope="row">' + newPizzaOrder.item + '</th><td id="size">' + $(".size option:selected").text() +'</td><td id="crust">' + $(".crust option:selected").text() +'</td><td id="toppings">' + $(".toppings option:selected").text() +'</td><td id="total">' + newPizzaOrder.totalPrice + '</td></tr>'
+        //appending order details to the table
+        console.log(newPizzaOrder.totalPrice);
+        $('.displayOrder').append(orderDetails);
+    });
+
+    });
 
     
 })
